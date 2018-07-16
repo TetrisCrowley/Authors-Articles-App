4a2400f8ffe7b4b0991524b7c211322816b3898c
Steve <sbrennison@gmail.com>
2018-07-16 12:06:47 -0500

    post route and index route are working

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/author');
 
 // author index route
 router.get('/', (req, res) => {
-  res.render('authors/index.ejs')
-})
+  Author.find({}, (err, foundAuthors) => {
+    res.render('authors/index.ejs', {
+     authors: foundAuthors
+    });
+  });
+});
 
 // author new route
 router.get('/new', (req, res) => {
   res.render('authors/new.ejs')
-})
+});
 
 // author create route
 router.post('/', (req, res) => {
   // see what user typed
   console.log(req.body)
-  // just to see if post route set up right
-  res.send('you posted, check terminal to see req.body');
-})
+  Author.create(req.body, (err, createdAuthor) => {
+    console.log(createdAuthor, ' this is the createdAuthor');
+    // just to see if post route set up right
+    res.redirect('/authors');
+  });
+});
 
 module.exports = router;
